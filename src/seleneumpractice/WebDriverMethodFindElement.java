0d174632269ad0e788def9cef43ad7d5b66b3b5b
Wankhede0414 <apekshawankhede11@gmail.com>
2023-05-15 22:04:00 +0530

    first commit

---
+++ src/seleneumpractice/WebDriverMethodFindElement.java
@@ -0,0 +1,19 @@
+package seleneumpractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverMethodFindElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.driver.chromdriver", "C:\\Users\\lenovo\\Downloads\\chrome\\chromedriver.exe");
+	
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.co.in/");
+	driver.findElement(By.name("q")).sendKeys("selenium");
+	
+	}
+
+}
